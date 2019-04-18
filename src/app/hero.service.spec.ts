90a9e85efaad093387604c9515af329c8782372f
mahesh <34829666+mvtram@users.noreply.github.com>
2019-04-18 21:21:22 +0530

    first angular task

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -5,21 +5,17 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
